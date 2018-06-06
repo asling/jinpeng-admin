50c0a5c187e72ab10650bc5d0723efd793ce9d56
chenyuwen@mindai.com <chenyuwen@mindai.com>
2018-06-06 13:44:21 +0800

    init

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,74 @@
+import DashboardPage from "views/Dashboard/Dashboard.jsx";
+import UserProfile from "views/UserProfile/UserProfile.jsx";
+import TableList from "views/TableList/TableList.jsx";
+import Typography from "views/Typography/Typography.jsx";
+import Icons from "views/Icons/Icons.jsx";
+import Maps from "views/Maps/Maps.jsx";
+import NotificationsPage from "views/Notifications/Notifications.jsx";
+import Employees from 'views/Employees/Employees.jsx';
+import Customers from 'views/Customers/Customers.jsx';
+
+import {
+  Dashboard,
+  Person,
+  ContentPaste,
+  LibraryBooks,
+  BubbleChart,
+  LocationOn,
+  Notifications
+} from "@material-ui/icons";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    sidebarName: "Dashboard",
+    navbarName: "Material Dashboard",
+    icon: Dashboard,
+    component: DashboardPage
+  },
+  {
+    path: "/user",
+    sidebarName: "User Profile",
+    navbarName: "Profile",
+    icon: Person,
+    component: UserProfile
+  },
+  {
+    path: "/employees",
+    sidebarName: "员工管理",
+    navbarName: "员工管理",
+    icon: ContentPaste,
+    component: TableList
+  },
+  {
+    path: "/typography",
+    sidebarName: "Typography",
+    navbarName: "Typography",
+    icon: LibraryBooks,
+    component: Typography
+  },
+  {
+    path: "/customers",
+    sidebarName: "客户管理",
+    navbarName: "客户管理",
+    icon: BubbleChart,
+    component: Customers
+  },
+  {
+    path: "/maps",
+    sidebarName: "Maps",
+    navbarName: "Map",
+    icon: LocationOn,
+    component: Maps
+  },
+  {
+    path: "/notifications",
+    sidebarName: "Notifications",
+    navbarName: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage
+  },
+  { redirect: true, path: "/", to: "/dashboard", navbarName: "Redirect" }
+];
+
+export default dashboardRoutes;
